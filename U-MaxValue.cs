39a0e6537f5a8883f9a047b7fd4aee33c0ef4500
Ira <105501017+iraska@users.noreply.github.com>
2022-06-21 21:42:20 +0300

    Create U-MaxValue.cs

---
+++ U-MaxValue.cs
@@ -0,0 +1,6 @@
+// Udemy - MaxValue
+// Write a program which takes two numbers from the console and displays the maximum of the two.
+
+// See https://aka.ms/new-console-template for more information
+var numbers = new int[] { 7, 77 };
+return numbers.Max();
