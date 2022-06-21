af1bd3dea9d9b895322232cb2ec9affe497f9e1e
Ira <105501017+iraska@users.noreply.github.com>
2022-06-21 21:18:01 +0300

    Create HR-ConditionalStatements.cs

---
+++ HR-ConditionalStatements.cs
@@ -0,0 +1,38 @@
+// Hacker Rank - Day 3 / Intro to conditional statements
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        int result = N%2;
+        if (result==1)
+        {
+            Console.WriteLine("Weird");
+        }
+        else
+        {
+            if(N >=2 && N <=5)    
+                Console.WriteLine("Not Weird");
+            else if(N >=6 && N <=20)
+                Console.WriteLine("Weird");
+            else if(N >20)
+                Console.WriteLine("Not Weird");
+        }
+    }
+}
