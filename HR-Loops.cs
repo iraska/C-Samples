9d4fc458db33692ed72a9d3c960d4e7d0d97ddd2
Ira <105501017+iraska@users.noreply.github.com>
2022-06-21 21:37:35 +0300

    Create HR-Loops.cs

---
+++ HR-Loops.cs
@@ -0,0 +1,29 @@
+// Hacker Rank - Day 5 / Loops
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        for(int i = 1; i <= 10; i++)
+        {
+            int result = n*i;
+            Console.WriteLine(n + " x " + i + " = " + result);
+        }
+    }
+}
